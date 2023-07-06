d02444d99306e01cdbc0c49655f01261bca58195
Lucas Costa <103662624+Lucascmo8@users.noreply.github.com>
2023-07-06 20:55:12 -0300

    Otimizando o Desafio 5
    - Fazendo algumas melhoras no Desafio 5

--- desafios/d005/analisador.php
+++ desafios/d005/analisador.php
@@ -10,17 +10,17 @@
     <main>
         <h1>Analisador de Número Real</h1>
         <?php 
-            $numeroReal = $_REQUEST["numeroReal"];
+            $numeroReal = $_REQUEST["numeroReal"] ?? 0;
             $numeroInteiro = intval($numeroReal);
-            $numeroFracionario = round(fmod($numeroReal,1),2);
+            $numeroFracionario = $numeroReal - $numeroInteiro;
 
-            echo "Analisando o número <strong>$numeroReal</strong> informado pelo usuário;";
+            echo "Analisando o número <strong>". number_format($numeroReal,3,",",".") ."</strong> informado pelo usuário;";
             echo "<ul>
-                <li>A parte inteira do número é <strong>$numeroInteiro</strong></li>
-                <li>A parte fracionária do número é <strong>$numeroFracionario</strong></li>
+                <li>A parte inteira do número é <strong>" . number_format($numeroInteiro,0,",",".") ."</strong></li>
+                <li>A parte fracionária do número é <strong>". number_format($numeroFracionario,3,",",".") ."</strong></li>
             </ul>";
         ?>
-        <button><a href="/">Voltar</a></button>
+        <button><a href="javascript:history.go(-1)">Voltar</a></button>
     </main>
 </body>
 </html>
